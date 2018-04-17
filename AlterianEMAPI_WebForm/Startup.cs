0f2b508f2308c2a3234c791338086fcc88bbdf1d
joe park <jpark@alterian.com>
2018-04-17 13:42:24 +1000

    Add project files.

---
+++ AlterianEMAPI_WebForm/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlterianEMAPI_WebForm.Startup))]
+namespace AlterianEMAPI_WebForm
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
